23622ed06f743267a5c590481224a20a52928046
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-10-02 05:07:31 -0600

    Refactor Footer component and update styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,19 @@
 import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="bg-black text-white py-10 mt-4">
       <div className="max-w-7xl mx-auto px-4">
         {/* Sección superior */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="flex flex-col space-y-2">
+          <div className="flex flex-col space-y-2 text-center lg:text-start">
             <a href="#" className="text-white hover:text-gray-400">Inicio</a>
             <a href="#" className="text-white hover:text-gray-400">Acerca de</a>
             <a href="#" className="text-white hover:text-gray-400">Tienda</a>
-            <a href="#" className="text-white hover:text-gray-400">Contacto</a>
           </div>
 
-          <div className="text-gray-400">
+          <div className="text-gray-400 text-[10px] text-center lg:text-start flex flex-col gap-[8px]">
             <p>José Domingo Lazo de la Vega s/n, Texcoco.</p>
             <p>+52 55 4025 2669</p>
             <p>example@boopsy.com</p>
@@ -22,11 +22,11 @@ const Footer = () => {
         </div>
 
         {/* Separador */}
-        <div className="border-t border-gray-700 my-6"></div>
+        <div className="hidden lg:block border-t border-gray-700 my-6"></div>
 
         {/* Sección inferior */}
         <div className="md:flex justify-between items-center text-sm text-gray-400">
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
+          <div className="hidden lg:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
             <span>Boopsy - México, 2024</span>
             <a href="#" className="hover:text-gray-200">Tabla de tallas</a>
             <a href="#" className="hover:text-gray-200">Aviso de Privacidad</a>
@@ -34,7 +34,7 @@ const Footer = () => {
           </div>
 
           {/* Redes sociales */}
-          <div className="flex space-x-4 mt-4 md:mt-0">
+          <div className="flex justify-center lg:justify-start space-x-4 mt-4 md:mt-0">
             <a href="#" className="text-white hover:text-gray-400">
               <FaFacebook size={24} />
             </a>
@@ -45,6 +45,8 @@ const Footer = () => {
               <FaTiktok size={24} />
             </a>
           </div>
+          <div className="lg:hidden border-t border-gray-700 my-6"></div>
+            <p className='text-center'>Boopsy - México, {year}</p>
         </div>
       </div>
     </footer>
