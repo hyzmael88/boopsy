ff551a8f91fe1ee29ed54f570aadbb89d04bacac
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-10-09 08:09:08 -0600

    Refactor Fit component: Update Fit section layout and add "Ver todas" link

--- src/components/Home/Fit.jsx
+++ src/components/Home/Fit.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { client } from '@/sanity/lib/client';
 import Image from "next/image";
+import Link from "next/link";
 
 
 const Categories = () => {
@@ -30,18 +31,23 @@ const Categories = () => {
     return (
       <section className="py-12">
         <h2 className="text-[20px] lg:text-[60px] font-anton uppercase text-center  mb-8">Fit</h2>
-        <div className="flex gap-6 px-4 ">
-          {fits.map((fit, index) => (
-            <div key={index} className="relative group overflow-hidden cursor-pointer ">
+        <div className="flex lg:justify-center gap-6 px-4 overflow-auto w-full  ">
+          {fits.slice(0,4).map((fit, index) => (
+            <div key={index} className="relative group overflow-hidden cursor-pointer flex-shrink-0">
               <Image 
               width={500}
               height={500}
               src={fit?.image?.asset?.url} alt={fit.name} className="w-[274px] h-[274px] object-cover group-hover:scale-110 transition-transform duration-300" />
               
-            <p className="text-center font-gabarito text-[20px] my-[25px] ">{fit.name}</p>
+            <p className="border-[0.5px] border-black/10 text-center font-gabarito text-[20px] py-[25px] ">{fit.name}</p>
             </div>
           ))}
         </div>
+        <div className='w-full h-[27px]  text-[16px] flex justify-center lg:justify-end mt-10'>
+                <Link href='/Shop' className='flex gap-2 font-gabarito'>
+                Ver todas <Image src='/assets/iconos/flecha.svg' width={17} height={17} alt='iconoflecha' />
+                </Link>
+              </div>
       </section>
     );
   };
