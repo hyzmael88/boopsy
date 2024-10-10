dc04262ff0e549f646b287ccab2269f77827a6b3
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-10-10 06:20:37 -0600

    Refactor: Add Error page component
    This commit adds a new component called Error, which is responsible for displaying an error message when there is a problem with the payment. The component includes a title, a description, and two buttons for navigating back to the cart or the home page.

---
+++ src/pages/Error.jsx
@@ -0,0 +1,30 @@
+// src/pages/Error.jsx
+import React from 'react';
+import Link from 'next/link';
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[70vh] bg-gray-100 p-4">
+      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl lg:text-4xl font-anton uppercase mb-4 ">¡Error en el Pago!</h1>
+        <p className="text-lg lg:text-xl text-gray-700 mb-6 font-gabarito">
+          Hubo un problema con tu pago. Por favor, revisa tu información o intenta de nuevo.
+        </p>
+        <div className="flex flex-col gap-4">
+          <Link href="/cart">
+            <button className="inline-block bg-black text-white px-20 py-2  text-lg lg:text-xl font-anton transition duration-300 uppercase rounded-[87px]">
+              Volver al Carrito
+            </button>
+          </Link>
+          <Link href="/">
+            <button className="inline-block bg-gray-400 text-white px-20 py-2  text-lg lg:text-xl font-anton transition duration-300 uppercase rounded-[87px]">
+              Volver al Inicio
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
